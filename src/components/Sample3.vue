415f0a86628c54e5870bc441af698c4094cab40c
Takahiro <tkhr_621@docomo.ne.jp>
2022-02-10 20:59:57 +0900

    クリックイベントのテスト

---
+++ src/components/Sample3.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Count is: {{ count }}</p>
+  <button @click="increment">+</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const data = ref(0)
+    const count = computed(() => data.value += 1)
+
+    return {
+      count
+    }
+  },
+})
+</script>
