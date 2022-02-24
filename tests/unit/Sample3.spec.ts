fdfb6420fa5e5bc2b581279c0339ba15b33e2a3d
Takahiro <tkhr_621@docomo.ne.jp>
2022-02-24 22:53:17 +0900

    修正

--- tests/unit/Sample3.spec.ts
+++ tests/unit/Sample3.spec.ts
@@ -2,9 +2,9 @@ import { mount } from '@vue/test-utils'
 import Sample3 from '../../src/components/Sample3.vue'
 
 describe('Sample3.vue', () => {
-    it('click', () => {
+    it('click', async () => {
         const wrapper = mount(Sample3)
-        wrapper.get('button').trigger('click')
+        await wrapper.get('button').trigger('click')
         expect(wrapper.text()).toContain('Count is: 1')
     })
 })
